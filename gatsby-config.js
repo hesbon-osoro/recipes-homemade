632b187b85c7637aa0598cb41a34c3e43314c052
wazimuhb <hesbonosoro1@gmail.com>
2021-11-26 02:35:01 +0300

    my details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,63 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `Recipes Homemade`,
+		description: `Recipes Homemade site using React, Node, Firebase, and Gatsby.`,
+		author: `@wazimuhb`,
+		siteUrl: `https://recipes-homemade-hb.netlify.app/`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-image`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: 'gatsby-firesource',
+			options: {
+				credential: require('./firebase.json'),
+				types: [
+					{
+						type: 'Recipe',
+						collection: 'recipes',
+						map: doc => ({
+							name: doc.name,
+							summary: doc.summary,
+							link: doc.link,
+							cook___NODE: doc.cook.id,
+						}),
+					},
+					{
+						type: 'Cook',
+						collection: 'cooks',
+						map: doc => ({
+							name: doc.name,
+						}),
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				// This will impact how browsers show your PWA/website
+				// https://css-tricks.com/meta-theme-color-and-trickery/
+				// theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
+};
