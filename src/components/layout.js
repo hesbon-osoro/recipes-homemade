6de01aaf72072e1a2cfdd29d3ef7a4b85e29506c
wazimuhb <hesbonosoro1@gmail.com>
2021-11-29 12:42:35 +0300

    updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,24 +26,22 @@ const Layout = ({ children }) => {
 	return (
 		<>
 			<Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-			<div
+			<main>{children}</main>
+			<footer
 				style={{
-					margin: `0 auto`,
-					maxWidth: 960,
-					padding: `0 1.0875rem 1.45rem`,
+					textAlign: 'center',
 				}}
 			>
-				<main>{children}</main>
-				<footer
-					style={{
-						marginTop: `2rem`,
-					}}
+				© {new Date().getFullYear()}, Built with
+				{` `}
+				<a
+					href="https://hb-wazimu.netlify.app/"
+					target="_blank"
+					rel="noopener noreferrer"
 				>
-					© {new Date().getFullYear()}, Built with
-					{` `}
-					<a href="https://www.gatsbyjs.com">Gatsby</a>
-				</footer>
-			</div>
+					Wazimu
+				</a>
+			</footer>
 		</>
 	);
 };
