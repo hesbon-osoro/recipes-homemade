d29456e6a4f076d6f15fcddf8553c742513a48e3
wazimuhb <hesbonosoro1@gmail.com>
2021-11-30 17:04:35 +0300

    remove unnecesary comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,15 +58,9 @@ module.exports = {
 				short_name: `starter`,
 				start_url: `/`,
 				background_color: `#663399`,
-				// This will impact how browsers show your PWA/website
-				// https://css-tricks.com/meta-theme-color-and-trickery/
-				// theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+				icon: `src/images/gatsby-icon.png`,
 			},
 		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
 	],
 };
